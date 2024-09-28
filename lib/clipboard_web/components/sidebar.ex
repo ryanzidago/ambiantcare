7916a4c6e58b250559e1dcad0188edc4883d13cb
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-09-29 00:09:26 +0200

    Made sidebar toggleable

--- lib/clipboard_web/components/sidebar.ex
+++ lib/clipboard_web/components/sidebar.ex
@@ -1,4 +1,7 @@
 defmodule ClipboardWeb.Sidebar do
+  @moduledoc """
+  https://flowbite.com/docs/components/sidebar/#default-sidebar
+  """
   use ClipboardWeb, :live_component
   use Gettext, backend: ClipboardWeb.Gettext
 
@@ -36,23 +39,53 @@ defmodule ClipboardWeb.Sidebar do
   @impl true
   def render(assigns) do
     ~H"""
-    <aside
-      id="default-sidebar"
-      class="w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-      aria-label="Sidenav"
-    >
-      <div class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-        <ul class="space-y-2">
+    <div class="flex flex-col gap-2">
+      <button
+        aria-controls="default-sidebar"
+        type="button"
+        class="inline-flex items-center p-2 mt-2 ms-3 text-sm w-10 rounded-lg hover:bg-gray-100 focus:outline-none dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        phx-click={
+          JS.toggle(
+            to: "#default-sidebar",
+            in: "transition ease-out duration-300",
+            out: "transition ease-in duration-300"
+          )
+        }
+      >
+        <span class="sr-only">Open sidebar</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          >
+          </path>
+        </svg>
+      </button>
+      <aside
+        id="default-sidebar"
+        class="w-64 h-screen transition-transform -translate-x-full sm:hidden md:block sm:translate-x-0"
+        aria-label="Sidenav"
+      >
+        <div class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
           <.branding />
-          <.locale_setting locale_options={@locale_options} locale={@locale} phxtarget={@myself} />
-          <.template_setting
-            template_options={@template_options}
-            template={@template}
-            phxtarget={@myself}
-          />
-        </ul>
-      </div>
-    </aside>
+          <div class="flex flex-col gap-2">
+            <.locale_setting locale_options={@locale_options} locale={@locale} phxtarget={@myself} />
+            <.template_setting
+              template_options={@template_options}
+              template={@template}
+              phxtarget={@myself}
+            />
+          </div>
+        </div>
+      </aside>
+    </div>
     """
   end
 
@@ -61,9 +94,9 @@ defmodule ClipboardWeb.Sidebar do
     <div class="justify-center p-4 rounded mb-20 shadow flex flex-row gap-0.5 drop-shadow-2xl font-semibold">
       <span class="text-blue-600">ambiant</span>
       <span class="bg-blue-600 px-2 rounded text-white flex flex-row items-end">
-      <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" class="mb-0.5">
-        <circle cx="3" cy="3" r="3" fill="white" />
-      </svg>
+        <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" class="mb-0.5">
+          <circle cx="3" cy="3" r="3" fill="white" />
+        </svg>
         Care
       </span>
     </div>
