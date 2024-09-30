701d1377ae372c329969f931bde785bdceb416cf
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-09-30 18:49:52 +0200

    Made sidebar smaller

--- lib/ambiantcare_web/components/sidebar.ex
+++ lib/ambiantcare_web/components/sidebar.ex
@@ -72,7 +72,7 @@ defmodule AmbiantcareWeb.Sidebar do
       </button>
       <aside
         id="default-sidebar"
-        class="w-64 h-screen transition-transform -translate-x-full sm:hidden md:block sm:translate-x-0"
+        class="w-44 h-screen transition-transform -translate-x-full sm:hidden md:block sm:translate-x-0"
         aria-label="Sidenav"
       >
         <div class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
