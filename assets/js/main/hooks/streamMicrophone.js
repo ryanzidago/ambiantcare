dbdcb02880d8e59c0c95829d0483790163decd7f
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-02 18:06:18 +0200

    Remove silent audio chunks

--- assets/js/main/hooks/streamMicrophone.js
+++ assets/js/main/hooks/streamMicrophone.js
@@ -1,6 +1,7 @@
 const CHUNK_DURATION_IN_MS = 5_000;
 const AUDIO_BITS_PER_SECOND = 128_000;
 const REQUEST_FIRST_CHUNK_AFTER_DURATION_IN_MS = 50;
+const SILENCE_THRESHOLD = 0.05;
 
 // @ryanzidago
 //
@@ -10,9 +11,8 @@ const REQUEST_FIRST_CHUNK_AFTER_DURATION_IN_MS = 50;
 // Otherwise, it isn't possible to read any but the very first chunk.
 const StreamMicrophone = {
   mounted() {
-    this.recording = false;
-    this.mediaRecorder = null;
-    this.firstBlob = null;
+    // @ryanzidago - ensure clean state on mount
+    this.stopRecording();
 
     this.el.addEventListener("click", () => {
       if (this.isRecording()) {
@@ -24,14 +24,12 @@ const StreamMicrophone = {
   },
 
   startRecording() {
-    this.audioChunks = [];
-
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       this.mediaRecorder = new MediaRecorder(stream, {
         audioBitsPerSecond: AUDIO_BITS_PER_SECOND,
       });
 
-      this.mediaRecorder.addEventListener("dataavailable", (event) => {
+      this.mediaRecorder.addEventListener("dataavailable", async (event) => {
         if (event.data.size > 0) {
           if (this.firstBlob && event.data.size < 25000) {
             return;
@@ -43,8 +41,14 @@ const StreamMicrophone = {
             const chunkWithHeader = new Blob([this.firstBlob, event.data], {
               type: event.type,
             });
-            this.audioChunks.push(chunkWithHeader);
-            this.processChunks();
+
+            const isSilent = await this.isSilent(chunkWithHeader);
+            if (isSilent) {
+              console.log("Silent chunk detected, skipping...");
+            } else {
+              this.audioChunks.push(chunkWithHeader);
+              this.processChunks();
+            }
           }
         }
       });
@@ -63,16 +67,21 @@ const StreamMicrophone = {
   },
 
   stopRecording() {
-    this.mediaRecorder.addEventListener("stop", () => {
-      this.processChunks();
-    });
+    if (this.mediaRecorder) {
+      this.mediaRecorder.addEventListener("stop", () => {
+        this.processChunks();
+      });
 
-    this.mediaRecorder.stop();
+      this.mediaRecorder.stop();
+    }
+
+    this.mediaRecorder = null;
     this.firstBlob = null;
+    this.audioChunks = [];
     clearInterval(this.updateInterval);
   },
 
-  processChunks() {
+  async processChunks() {
     if (this.audioChunks.length < 1) return;
 
     const audioBlob = new Blob(this.audioChunks);
@@ -82,6 +91,24 @@ const StreamMicrophone = {
 
   isRecording() {
     return this.mediaRecorder && this.mediaRecorder.state === "recording";
+  },
+
+  isSilent(audioChunk) {
+    return new Promise((resolve) => {
+      const audioContext = new (window.AudioContext ||
+        window.webkitAudioContext)();
+      const fileReader = new FileReader();
+
+      fileReader.onload = function () {
+        audioContext.decodeAudioData(this.result, (buffer) => {
+          const channelData = buffer.getChannelData(0);
+          const amplitude = Math.max(...channelData.map(Math.abs));
+          resolve(amplitude <= SILENCE_THRESHOLD);
+        });
+      };
+
+      fileReader.readAsArrayBuffer(audioChunk);
+    });
   },
 };
 
