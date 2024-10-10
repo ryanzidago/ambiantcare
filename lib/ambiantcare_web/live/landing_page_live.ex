05b6d8522075127a95757d0b33b902735d67f601
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-10 21:36:18 +0200

    Added Italian translations

--- lib/ambiantcare_web/live/landing_page_live.ex
+++ lib/ambiantcare_web/live/landing_page_live.ex
@@ -235,7 +235,7 @@ defmodule AmbiantcareWeb.LandingPageLive do
       <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
         <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Our Team
+            <%= gettext("Our Team") %>
           </h2>
           <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400"></p>
         </div>
