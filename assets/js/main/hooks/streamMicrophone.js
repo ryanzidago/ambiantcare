154b4670dda982146c4150048239e01c705f4077
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-12 22:35:48 +0200

    Refactoring of StreamMicrophone + improved logging + increase size of audio chunks

--- assets/js/main/hooks/streamMicrophone.js
+++ assets/js/main/hooks/streamMicrophone.js
@@ -1,7 +1,12 @@
-const CHUNK_DURATION_IN_MS = 5_000;
+// the duration of the audio chunk in milliseconds
+// there's a bug currently where audio chunks are twice as long as they should be
+const CHUNK_DURATION_IN_MS = 1_000 * 60 * 1;
 const AUDIO_BITS_PER_SECOND = 128_000;
+// the duration in milliseconds after which the first chunk is requested
+// this determines the size of the first chunk
 const REQUEST_FIRST_CHUNK_AFTER_DURATION_IN_MS = 50;
 const SILENCE_THRESHOLD = 0.05;
+const SAMPLE_RATE = 16_000;
 
 // @ryanzidago
 //
@@ -9,6 +14,7 @@ const SILENCE_THRESHOLD = 0.05;
 // Because .ogg can't be streamed, we need to build the chunks ourselves.
 // I'm using the first chunk that contains the headers and prepend it to the rest of the chunks.
 // Otherwise, it isn't possible to read any but the very first chunk.
+// See Elixir forum thread here: https://elixirforum.com/t/how-to-stream-audio-chunks-from-the-browser-to-the-server/66091
 const StreamMicrophone = {
   mounted() {
     console.log("StreamMicrophone mounted");
@@ -16,12 +22,26 @@ const StreamMicrophone = {
     this.stopRecording();
 
     this.el.addEventListener("click", () => {
-      if (this.isRecording()) {
+      if (isRecording(this.mediaRecorder)) {
         this.stopRecording();
       } else {
         this.startRecording();
       }
     });
+  },
+
+  updated() {
+    console.log("StreamMicrophone updated");
+  },
+
+  destroyed() {
+    console.log("StreamMicrophone destroyed");
+    this.stopRecording();
+  },
+
+  disconnected() {
+    console.log("StreamMicrophone disconnected");
+    this.stopRecording();
   },
 
   startRecording() {
@@ -48,6 +68,8 @@ const StreamMicrophone = {
 
   handleOnDataAvailable() {
     this.mediaRecorder.addEventListener("dataavailable", async (event) => {
+      console.log("Data available", event);
+
       if (event.data.size > 0) {
         if (this.firstBlob && event.data.size < 25000) {
           return;
@@ -60,8 +82,11 @@ const StreamMicrophone = {
             type: event.type,
           });
 
-          const isSilent = await this.isSilent(chunkWithHeader);
-          if (isSilent) {
+          this.audioChunks.push(chunkWithHeader);
+          this.processChunks();
+
+          const isSilentChunk = await isSilent(chunkWithHeader);
+          if (isSilentChunk) {
             console.log("Silent chunk detected, skipping...");
           } else {
             this.audioChunks.push(chunkWithHeader);
@@ -86,8 +111,11 @@ const StreamMicrophone = {
   },
 
   stopRecording() {
+    console.log("Stop recording");
     if (this.mediaRecorder) {
+      // once `this.mediaRecorder.stop()` is called, the `stop` event is triggered
       this.mediaRecorder.addEventListener("stop", () => {
+        console.log("Stop recording - processing the last chunks");
         this.processChunks();
       });
 
@@ -98,38 +126,122 @@ const StreamMicrophone = {
     this.firstBlob = null;
     this.audioChunks = [];
     clearInterval(this.updateInterval);
-    // this.pushEvent("stop_recording", {});
+    this.pushEvent("stop_recording", {});
   },
 
   async processChunks() {
+    console.log("Process chunks");
+    console.log(this.audioChunks.length);
+
     if (this.audioChunks.length < 1) return;
 
     const audioBlob = new Blob(this.audioChunks);
-    this.upload("audio_from_user_microphone", [audioBlob]);
+    console.log(audioBlob);
+
+    audioBlob.arrayBuffer().then((buffer) => {
+      const context = new AudioContext({ sampleRate: SAMPLE_RATE });
+
+      context.decodeAudioData(buffer, (audioBuffer) => {
+        const pcmBuffer = audioBufferToPcm(audioBuffer);
+        const buffer = convertEndianness32(
+          pcmBuffer,
+          getEndianness(),
+          this.el.dataset.endianness
+        );
+        this.upload("audio_from_user_microphone", [new Blob([buffer])]);
+      });
+    });
+
     this.audioChunks = [];
   },
-
-  isRecording() {
-    return this.mediaRecorder && this.mediaRecorder.state === "recording";
-  },
-
-  isSilent(audioChunk) {
-    return new Promise((resolve) => {
-      const audioContext = new (window.AudioContext ||
-        window.webkitAudioContext)();
-      const fileReader = new FileReader();
-
-      fileReader.onload = function () {
-        audioContext.decodeAudioData(this.result, (buffer) => {
-          const channelData = buffer.getChannelData(0);
-          const amplitude = Math.max(...channelData.map(Math.abs));
-          resolve(amplitude <= SILENCE_THRESHOLD);
-        });
-      };
-
-      fileReader.readAsArrayBuffer(audioChunk);
-    });
-  },
 };
+
+function isRecording(mediaRecorder) {
+  return mediaRecorder && mediaRecorder.state === "recording";
+}
+
+function isSilent(audioChunk) {
+  return new Promise((resolve) => {
+    const audioContext = new (window.AudioContext ||
+      window.webkitAudioContext)();
+    const fileReader = new FileReader();
+
+    fileReader.onload = function () {
+      audioContext
+        .decodeAudioData(this.result)
+        .then((buffer) => {
+          const channelData = buffer.getChannelData(0);
+          const maxAmplitude = channelData.reduce(
+            (max, value) => Math.max(max, Math.abs(value)),
+            0
+          );
+          resolve(maxAmplitude <= SILENCE_THRESHOLD);
+        })
+        .catch((error) => {
+          console.error("Errror decoding audio data", error);
+          resolve(false);
+        });
+    };
+
+    fileReader.readAsArrayBuffer(audioChunk);
+  });
+}
+
+function audioBufferToPcm(audioBuffer) {
+  const numChannels = audioBuffer.numberOfChannels;
+  const length = audioBuffer.length;
+  const size = Float32Array.BYTES_PER_ELEMENT * length;
+  const buffer = new ArrayBuffer(size);
+  const pcmArray = new Float32Array(buffer);
+  const channelDataBuffers = Array.from({ length: numChannels }, (x, channel) =>
+    audioBuffer.getChannelData(channel)
+  );
+
+  // Average all channels upfront, so the PCM is always mono
+  for (let i = 0; i < pcmArray.length; i++) {
+    let sum = 0;
+
+    for (let channel = 0; channel < numChannels; channel++) {
+      sum += channelDataBuffers[channel][i];
+    }
+
+    pcmArray[i] = sum / numChannels;
+  }
+
+  return buffer;
+}
+
+function convertEndianness32(buffer, from, to) {
+  if (from === to) return buffer;
+
+  // If the endianness differs, we swap bytes accordingly
+  for (let i = 0; i < buffer.byteLength / 4; i++) {
+    const b1 = buffer[i];
+    const b2 = buffer[i + 1];
+    const b3 = buffer[i + 2];
+    const b4 = buffer[i + 3];
+
+    buffer[i] = b4;
+    buffer[i + 1] = b3;
+    buffer[i + 2] = b2;
+    buffer[i + 3] = b1;
+  }
+
+  return buffer;
+}
+
+function getEndianness() {
+  const buffer = new ArrayBuffer(2);
+  const int16Array = new Uint16Array(buffer);
+  const int8Array = new Uint8Array(buffer);
+
+  int16Array[0] = 1;
+
+  if (int8Array[0] === 1) {
+    return "little";
+  } else {
+    return "big";
+  }
+}
 
 export { StreamMicrophone };
