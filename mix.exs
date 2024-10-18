65586464f30270f27f99a5226dbfcbec8ebe01aa
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-18 21:20:51 +0200

    Setup auth

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule Ambiantcare.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:argon2_elixir, "~> 3.0"},
       {:phoenix, "~> 1.7.14"},
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
