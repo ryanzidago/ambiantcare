b32fec3dfcb1a4bb6943699d3b59ca795a25176b
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-19 18:02:23 +0200

    Revert "Setup docker compose" (#7)

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :ambiantcare, Ambiantcare.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: System.get_env("DATABASE_HOSTNAME", "localhost"),
+  hostname: "localhost",
   database: "ambiantcare_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
@@ -19,7 +19,7 @@ config :ambiantcare, Ambiantcare.Repo,
 config :ambiantcare, AmbiantcareWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
