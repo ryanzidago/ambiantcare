cd4e7500796ba8f978819b20c05d21071d4e5ea8
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-10-26 23:05:51 +0200

    Added consultation schema

---
+++ lib/ambiantcare/consultations.ex
@@ -0,0 +1,30 @@
+defmodule Ambiantcare.Consultations do
+  alias Ambiantcare.Consultations.Consultation
+  alias Ambiantcare.Accounts.User
+  alias Ambiantcare.Repo
+
+  alias Ecto.Changeset
+
+  import Ecto.Query
+
+  @spec create_or_update_consultation(User.t(), Consultation.t(), map()) ::
+          {:ok, Consultation.t()}
+          | {:error, Changeset.t()}
+  def create_or_update_consultation(
+        %User{} = _current_user,
+        %Consultation{} = consultation,
+        %{} = attrs
+      ) do
+    consultation
+    |> Consultation.changeset(attrs)
+    |> Repo.insert_or_update()
+  end
+
+  @spec get_consultation(User.t(), Ecto.UUID.t()) :: Consultation.t() | nil
+  def get_consultation(%User{} = user, id) do
+    from(cons in Consultation, as: :cons)
+    |> where([cons: cons], cons.user_id == ^user.id)
+    |> where([cons: cons], cons.id == ^id)
+    |> Repo.one()
+  end
+end
