0554c0fec7c16bbda35f39cdfc793e4f31a7217a
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-11-04 17:26:25 +0100

    Removed picturs from lp (#33)
    * Removed picturs from lp
    
    * Removed envelop icon

--- lib/ambiantcare_web/components/core_components.ex
+++ lib/ambiantcare_web/components/core_components.ex
@@ -632,7 +632,7 @@ defmodule AmbiantcareWeb.CoreComponents do
 
   def icon(%{name: "fa-linkedin"} = assigns) do
     ~H"""
-    <svg class={["w-5 h-5", @class]} fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+    <svg class={["w-full h-5", @class]} fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
       <path
         fill-rule="evenodd"
         d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22 3.5C22 2.672 21.328 2 20.5 2h-17C2.672 2 2 2.672 2 3.5v17c0 .828.672 1.5 1.5 1.5h17c.828 0 1.5-.672 1.5-1.5v-17z"
