c2ca31f973c99b2cc7030a3545974e42b8947475
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-11-10 19:26:24 +0100

    Cleaned up cloack configs (#42)

--- lib/ambiantcare/vault.ex
+++ lib/ambiantcare/vault.ex
@@ -1,26 +1,3 @@
 defmodule Ambiantcare.Vault do
   use Cloak.Vault, otp_app: :ambiantcare
-
-  @impl GenServer
-  def init(config) do
-    ciphers = [
-      default: {
-        Cloak.Ciphers.AES.GCM,
-        tag: "AES.GCM.V1", key: decode_env!("CLOAK_ENCRYPTION_KEY"), iv_length: 12
-      }
-    ]
-
-    config =
-      config
-      |> Keyword.put(:ciphers, ciphers)
-      |> Keyword.put(:json_library, Jason)
-
-    {:ok, config}
-  end
-
-  defp decode_env!(var) do
-    var
-    |> System.fetch_env!()
-    |> Base.decode64!()
-  end
 end
